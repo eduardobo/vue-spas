178fde19d7f186d038601226fb29bf0aff26a798
Eduardo Barron <jbarron@empleosti.com.mx>
2021-05-10 10:52:42 -0500

    add content and css to LoadersAndAnimations.vue

--- resources/js/components/LoadersAndAnimations.vue
+++ resources/js/components/LoadersAndAnimations.vue
@@ -1,13 +1,31 @@
 <template>
     <div>
-        <p>Loaders & Animations</p>
+        <h1 class="font-normal text-3xl gray-dark-font leading-none mb-8">Loaders and Animations</h1>
+
+        <p>This spinner uses AirBnb's <a href="http://airbnen.io/lotti/#/web" class="text-blue font-bold">Lottie</a>
+            library to render its animation. You may download the necessary assets below</p>
+        <div class="border rounded-lg flex align-items-center justify-center mb-8">
+            <div ref="lottie" class="w-64" style="height: 457px"></div>
+        </div>
+
+        <a href="/images/logo.svg"
+            class="border px-10 py-2 rounded-full text-black-50 font-bold text-xs"
+        >spinner.zip</a>
     </div>
 </template>
 
 <script>
+    import lottie from 'lottie-web';
+
     export default {
         mounted() {
-            console.log('Component mounted.')
+            lottie.loadAnimation({
+                container: this.$refs.lottie,
+                renderer: 'svg',
+                loop: true,
+                autoplay: true,
+                path: '/animations/spinner.json'
+            })
         }
     }
 </script>
