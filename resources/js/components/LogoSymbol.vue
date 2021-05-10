0c114386c972f48e652c574a74159e841dd15dcf
Eduardo Barron <jbarron@empleosti.com.mx>
2021-05-10 10:52:19 -0500

    add content and css to logosymbol.vue

--- resources/js/components/LogoSymbol.vue
+++ resources/js/components/LogoSymbol.vue
@@ -1,6 +1,31 @@
 <template>
     <div>
-        <p>Logo Symbol</p>
+        <h1 class="font-normal text-3xl gray-dark-font leading-none mb-8">Logo Symbol</h1>
+        <div>
+            <div class="mb-8">
+                <picture style="height: 382px" class="block shadow rounded-lg flex align-items-center justify-center mb-6">
+                    <img src="/images/symbol-original.svg">
+                </picture>
+
+                <div class="center flex justify-content-between align-items-center">
+                    <p>Original symbol on <strong>white</strong> background</p>
+
+                    <a href="/images/symbol-original.svg" class="border border-gray-800 px-3 py-1 rounded-full text-black font-bold text-xs">symbol-original.svg</a>
+                </div>
+            </div>
+
+            <div class="mb-8">
+                <picture style="height: 382px" class="block shadow rounded-lg flex align-items-center justify-center mb-6 bg-blue">
+                    <img src="/images/symbol-negative.svg">
+                </picture>
+
+                <div class="center flex justify-content-between align-items-center">
+                    <p>Original symbol on <strong>white</strong> background</p>
+
+                    <a href="/images/symbol-negative.svg" class="border border-gray-800 px-3 py-1 rounded-full text-black font-bold text-xs">symbol-negative.svg</a>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
