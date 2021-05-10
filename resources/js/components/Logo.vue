ce56d2d89614c17af4219a3e66ab3ed82d26a405
Eduardo Barron <jbarron@empleosti.com.mx>
2021-05-10 09:54:53 -0500

    add content and css to logo.vue

--- resources/js/components/Logo.vue
+++ resources/js/components/Logo.vue
@@ -1,6 +1,29 @@
 <template>
     <div>
-        <p>Logo</p>
+        <h1 class="font-normal text-3xl gray-dark-font leading-none mb-8">Full Logo</h1>
+        <div class="mb-8">
+            <picture style="height: 382px" class="block shadow rounded-lg flex align-items-center justify-center mb-6">
+               <img src="/images/logo.svg" alt="logo">
+            </picture>
+
+            <div class="flex justify-content-between align-items-center">
+                <p>Original Logo on <strong>white</strong> background</p>
+
+                <a href="/images/logo.svg" class="border border-gray-800 px-8 py-1 rounded-full text-black font-bold text-xs">logo.svg</a>
+            </div>
+        </div>
+
+        <div class="mb-8">
+            <picture style="height: 382px" class="block shadow rounded-lg flex align-items-center justify-center mb-6 bg-blue">
+                <img src="/images/negative-logo.svg" alt="logo">
+            </picture>
+
+            <div class="flex justify-content-between align-items-center">
+                <p>Negative Logo on <strong>white</strong> background</p>
+
+                <a href="/images/negative-logo.svg" class="border border-gray-800 px-8 py-1 rounded-full text-black font-bold text-xs">negative-logo.svg</a>
+            </div>
+        </div>
     </div>
 </template>
 
